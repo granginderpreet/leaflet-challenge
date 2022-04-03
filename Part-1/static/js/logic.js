79085d298f74644c2014fe03dd04be76e48f9961
Inderpreet Grang <granginderpreet@gmail.com>
2022-04-03 02:17:15 -0700

    Pseudo final version for Part1. Part 2 is still not finished

--- Part-1/static/js/logic.js
+++ Part-1/static/js/logic.js
@@ -19,18 +19,41 @@ function createMap(locations) {
     };
   
     // Create the map object with options.
-     map = L.map("map", {
-    center: [40.73, -90.0059],
-    zoom: 6,
+     var map = L.map("map", {
+    center: [40.73, -110.0059],
+    zoom: 4,
+  
      layers: [streetmap, locations]
   });
 
-   locations.addTo(map);
+   //locations.addTo(map);
 
     //Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(map);
+    {
+
+      var legend = L.control({position: 'bottomleft'});
+      legend.onAdd = function(map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+          grades = [-10,0,1,5,10,20,50,100],
+          labels = ['<strong>Depth (in km.) below the surface</strong>'],
+          from, to;
+        for (var i = 0; i < grades.length; i++) {
+          from = grades[i];
+          to = grades[i + 1];
+          div.innerHTML +=
+          labels.push(
+            '<i class="circle" style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+        }
+        div.innerHTML = labels.join('<br>');
+        console.log(div)
+        return div;
+      };
+      legend.addTo(map);
+    }
 }
 
 
@@ -39,10 +62,18 @@ function markerSize(population) {
   return Math.sqrt(population) * 50;
 }
 
+function getColor(d) {
+  // We have changed the Red color in legend to map the least depth since that can cause more damage 
+  var mapScale = chroma.scale(['#FF0000','#bcf831'])
+    .classes([-10,0,1,5,10,20,50,100]);
+  return mapScale(d)
+}
+
+// Pull the "stations" property from response.data.
 
   function createMarkers(response) {
   
-    // Pull the "stations" property from response.data.
+
     var metadata = response.metadata;
   
     // Initialize an array to hold bike markers.
@@ -52,28 +83,32 @@ function markerSize(population) {
     for (var index = 0; index < metadata.count; index++) {
       var loc = response.features[index].geometry.coordinates;
       var mag = response.features[index].properties.mag;
-      console.log( markerSize(response.features[index].properties.mag), response.features[index].properties.place)
+    //  console.log( markerSize(response.features[index].properties.mag), response.features[index].properties.place)
       
-      if (isNaN(mag) || isNaN(loc)) 
+      if (loc[2]< -10) 
       {
-        console.log("mag=",mag,"loc=",loc)
-        continue;
+        console.log("mag=",mag,"depth outside our range",loc)
       }
       // For each station, create a marker, and bind a popup with the station's name.
-     var marker=L.circleMarker([loc[0],loc[1]],{
-        fillOpacity: 0.75,
-        color: "white",
-        fillColor: "purple",
+     var marker=L.circleMarker([loc[1],loc[0]],{
+        fillOpacity: 0.5,
+        color: "black",
+        weight:0.1,
+        fillColor: getColor(loc[2]),
         // Setting our circle's radius to equal the output of our markerSize() function:
         // This will make our marker's size proportionate to its population.
-       radius: 10*response.features[index].properties.mag}).bindPopup(`<h3> ${mag} seen at ${response.features[index].properties.place}</h3>`);
+       radius: 2*response.features[index].properties.mag}).bindPopup(`<h3> Magnitude ${mag} seen at ${response.features[index].properties.place} at ${loc[2]} km below earth surface </h3>`);
        
        markers.push(marker);
-       console.log( markers)
+       //console.log( getColor(loc[2]))
 
      }
      createMap(L.layerGroup(markers));
     }
   var map;
+
+
+  
+
   // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
   d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # leaflet-challenge
+The main file is logic.js. Here we load D3 .json and get  the response fed into the fucntion that created the tiles layer, and extracts the long, lat and depth. The pop up windows for each locatiomn
+were added that would report the detaild of the quake. In additoin we added depth to track via color of the bubble while radius tracks the magnitude of quake.
+
+NOTE we decided to keep the earthquakes close to surface as more impact full to ones deep ones. Hence the color changes from Red (zero depth) to Green (large depth)
